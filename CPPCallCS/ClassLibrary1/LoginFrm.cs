29c94396e354bc437f4a3290ba54e55d3fcafb04
Chen Yingjing <34758985+yingjingchen-pki@users.noreply.github.com>
2018-04-10 12:57:53 +0800

    Added a user and changed password.

--- CPPCallCS/ClassLibrary1/LoginFrm.cs
+++ CPPCallCS/ClassLibrary1/LoginFrm.cs
@@ -59,6 +59,10 @@ namespace ClassLibrary1
             {
                 DialogResult = DialogResult.OK;
             }
+            else if (UserNameTextBox.Text == "cyj11" && PasswordTextBox.Text == "1234567")
+            {
+                DialogResult = DialogResult.OK;
+            }
             else
             {
                 MessageBox.Show("User Name or Password is wrong.");
