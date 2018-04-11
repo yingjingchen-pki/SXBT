3882fc86b482902826be8804b16069b314741dd2
Chen Yingjing <34758985+yingjingchen-pki@users.noreply.github.com>
2018-04-11 09:38:07 +0800

    Added a witesite to generate the key.

---
+++ CPPCallCS/KeyGeneraterWebApp/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace KeyGeneraterWebApp.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CPPCallCS/KeyGeneraterWebApp/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using SXPWDLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KeyGeneraterWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void generatePassButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            if (String.IsNullOrEmpty(username))
+            {
+                passwordTextBox.Text = "Please input user name.";
+                return;
+            }
+            string password = Authentication.GeneratePasswordByName(username);
+            passwordTextBox.Text = password;
+
+        }
+    }
+}
